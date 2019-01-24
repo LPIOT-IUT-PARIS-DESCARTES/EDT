8dd43324b620b30bbca5ab3567a8baf4d39644c7
unknown <ayazbulls@gmail.com>
2019-01-24 15:57:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<?php
+	if(isset($_GET['controle']) & isset($_GET['action']) & isset($_GET['id'])){
+		try {
+			$controle = $_GET['controle'];
+
+			if (!file_exists('./controller/' . $controle . '.php')){
+				throw new Exception("Require doesn't exist");
+			} else {
+				require ('./controller/' . $controle . '.php');
+				$cont = new $controle();
+				$action= $_GET['action'];
+				$id=$_GET['id'];
+				$cont->{$action}($id); 
+			}
+		} catch (Exception $e){
+			require("vue/erreur404.html");
+		}
+	}
+	elseif (isset($_GET['controle']) & isset($_GET['action'])) {
+		try {
+			$controle = $_GET['controle'];
+
+			if (!file_exists('./controller/' . $controle . '.php')){
+				throw new Exception("Require doesn't exist");	
+			} else {
+				require ('./controller/' . $controle . '.php');
+				$cont = new $controle();	
+				$action= $_GET['action'];
+				$cont->{$action}();
+			} 
+
+		} catch(Exception $e) {
+			require("vue/404.html");
+		}
+	} else {
+		require("vue/layout/edt.html");
+	}
+?>
